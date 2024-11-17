19f379c24d48ae0e03f8a83db48c6e21598cf0ff
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-11-17 20:38:07 -0300

    7 testes

--- MusicTestes.testes/MusicsServicesTestes.cs
+++ MusicTestes.testes/MusicsServicesTestes.cs
@@ -29,19 +29,17 @@ namespace MusicTestes.testes
             // Act & Assert
             sut.AddMusic(music);
         }
-
         public static IEnumerable<object[]> GetMusicsData()
         {
-            var faker = new Faker();
-
+            Faker faker = new Faker();
             yield return new object[] { new Music()
-                {
-                    Nome = faker.Lorem.Word(),
-                    Artista = faker.Person.FullName,
-                    AnoDeLancamento = faker.Random.Int(1980, 1999),
-                    Classificacao = faker.Random.Double(3.2, 5.5),
-                    Genero = Genero.Rock
-                }
+            {
+                Nome = faker.Lorem.Word(),
+                Artista = faker.Person.FullName,
+                AnoDeLancamento = faker.Random.Int(1980, 1999),
+                Classificacao = faker.Random.Double(3.2, 5.5),
+                Genero = Genero.Rock
+            }
             };
             yield return new object[] { new Music()
                 {
@@ -52,6 +50,59 @@ namespace MusicTestes.testes
                     Genero = Genero.RockClassico
                 }
             };
+            yield return new object[] { new Music()
+                {
+                    Nome = faker.Lorem.Word(),
+                    Artista = faker.Person.FullName,
+                    AnoDeLancamento = faker.Random.Int(1980, 1999),
+                    Classificacao = faker.Random.Double(8.0, 10.0),
+                    Genero = Genero.HeavyMetal
+                }
+            };
+        }
+        [Fact]
+        public void AddMusic_DeveLancarException_QuandoMusicaNula()
+        {
+            // Arrange
+            MusicaService sut = new MusicaService();
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => sut.AddMusic(null));
+        }
+        [Fact]
+        public void AddMusic_DeveLancarFormatException_QuandoNomeInvalido()
+        {
+            // Arrange
+            MusicaService sut = new MusicaService();
+            var music = new Music()
+            {
+                Nome = "A",
+                Artista = _faker.Person.FullName,
+                AnoDeLancamento = 1990,
+                Classificacao = 5.0,
+                Genero = Genero.Rock
+            };
+
+            // Act & Assert
+            Assert.Throws<FormatException>(() => sut.AddMusic(music));
+        }
+
+        [Fact]
+        public void AddMusic_DeveLancarException_QuandoAnoDeLancamentoInvalido()
+        {
+            // Arrange
+            MusicaService sut = new MusicaService();
+            var music = new Music()
+            {
+                Nome = _faker.Lorem.Word(),
+                Artista = _faker.Person.FullName,
+                AnoDeLancamento = 2020,
+                Classificacao = 6.0,
+                Genero = Genero.RockClassico
+            };
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => sut.AddMusic(music));
         }
     }
 }
